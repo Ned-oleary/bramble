af9155b2318c33ef820e9e463daa927c757ac198
ned-oleary <edwardkoleary@gmail.com>
2024-07-03 19:09:57 -0700

    minor apollo refactor

--- app/routes/apollo_routes.py
+++ app/routes/apollo_routes.py
@@ -1,49 +1,43 @@
 from flask import Blueprint, jsonify, request
 from typing import Tuple, TYPE_CHECKING
-from dotenv import load_dotenv
 from ..models.person import generate_test_person
-from ..utils.apollo_utils import PEOPLE_MATCH_VALID_INPUTS, PEOPLE_MATCH_URI, PEOPLE_MATCH_URI_BULK, COMPANY_MATCH_URI, COMPANY_MATCH_URI_BULK
-import requests, os
-
-load_dotenv()
+from ..utils.apollo_utils import PEOPLE_MATCH_VALID_INPUTS, PEOPLE_MATCH_URI, \
+                                PEOPLE_MATCH_URI_BULK, COMPANY_MATCH_URI, \
+                                COMPANY_MATCH_URI_BULK, MATCH_HEADERS_NO_JSON, MATCH_HEADERS_JSON
+import requests
 
 if TYPE_CHECKING:
     from flask import Flask
     from ..models.person import Person, generate_test_person
 
-PEOPLE_MATCH_HEADERS : dict = {'Cache-Control': 'no-cache', 
-                        'X-Api-Key': os.getenv("APOLLO_KEY")}
-
-PEOPLE_MATCH_BULK_HEADERS : dict = {'Content-Type': 'application/json', 
-                                    'Cache-Control': 'no-cache', 
-                                    'X-Api-Key': os.getenv("APOLLO_KEY")}
-
 bp: Blueprint = Blueprint("apollo", import_name="apollo_routes", url_prefix="/api/apollo")
 
+# processes 10 max at a time
 @bp.route("/enrich/person", methods=["POST"])
 def enrich_person() -> Tuple[dict, int]:
     data = request.get_json()
 
     if data["choice"] == "bulk":
         payload_dict = {"details": data["details"]}  
-        response = requests.post(url=PEOPLE_MATCH_URI_BULK, headers=PEOPLE_MATCH_BULK_HEADERS, json=payload_dict)
+        response = requests.post(url=PEOPLE_MATCH_URI_BULK, headers=MATCH_HEADERS_JSON, json=payload_dict)
     else:
-        response = requests.post(url=PEOPLE_MATCH_URI, headers=PEOPLE_MATCH_HEADERS, json=data["details"])  
+        response = requests.post(url=PEOPLE_MATCH_URI, headers=MATCH_HEADERS_NO_JSON, json=data["details"])  
 
     if response.status_code == 200:  # Use 200 for clarity, although requests.codes.ok is also correct
         return jsonify(response.json()), 201
     else:
         return response.text, response.status_code
 
+# processes 10 max at a time
 @bp.route("/enrich/company", methods = ["POST"])
 def enrich_company() -> Tuple[dict, int]:
-    data = request.get_json()  # Correct way to get JSON data
-    payload = {"domains": data["domains"]}  # Ensure payload is correctly formatted as JSON
+    data = request.get_json()  
+    payload = {"domains": data["domains"]}
 
     if data["choice"] == "bulk":
-        response = requests.post(url=COMPANY_MATCH_URI_BULK, headers=PEOPLE_MATCH_BULK_HEADERS, json=payload)
+        response = requests.post(url=COMPANY_MATCH_URI_BULK, headers=MATCH_HEADERS_JSON, json=payload)
     else:
-        response = requests.get(url=COMPANY_MATCH_URI, headers=PEOPLE_MATCH_BULK_HEADERS, params={"domain": data["domains"][0]})
+        response = requests.get(url=COMPANY_MATCH_URI, headers=MATCH_HEADERS_JSON, params={"domain": data["domains"][0]})
 
     if response.status_code == 200:
         return jsonify(response.json()), 201
--- app/utils/apollo_utils.py
+++ app/utils/apollo_utils.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 PEOPLE_MATCH_VALID_INPUTS = ["first_name", "last_name", "name", "email", 
                              "hashed_email", "organization_name", "domain",
                              "linkedin_url"]
@@ -7,4 +12,11 @@ PEOPLE_MATCH_URI_BULK = "https://api.apollo.io/api/v1/people/bulk_match"
 
 COMPANY_MATCH_URI = "https://api.apollo.io/v1/organizations/enrich"
 COMPANY_MATCH_URI_BULK = "https://api.apollo.io/api/v1/organizations/bulk_enrich"
+
+MATCH_HEADERS : dict = {'Cache-Control': 'no-cache', 
+                        'X-Api-Key': os.getenv("APOLLO_KEY")}
+
+MATCH_HEADERS_BULK : dict = {'Content-Type': 'application/json', 
+                                    'Cache-Control': 'no-cache', 
+                                    'X-Api-Key': os.getenv("APOLLO_KEY")}
 
