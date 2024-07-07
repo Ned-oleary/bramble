669cd231c35e06307ce79c2cd4df5f4a0ee813f9
Ned O'Leary <edwardkoleary@gmail.com>
2024-07-07 08:05:07 -0700

    continued WIP refactor

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from .models import init_models # may be able to remove this
 from .routes import register_routes
 from .utils import init_utils
 from .utils import init_utils
@@ -9,7 +8,6 @@ from flask_cors import CORS
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object("config.Config")
-    init_models(app)
     init_utils(app)
     register_routes(app)
     CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:3000"}})
