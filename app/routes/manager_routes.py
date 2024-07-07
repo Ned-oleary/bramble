33e230dfdd9ae48f44c2cb7d666ff7bf1257dbf4
Ned O'Leary <edwardkoleary@gmail.com>
2024-07-07 08:06:53 -0700

    continued WIP refactor: backend json manipulation

--- app/routes/manager_routes.py
+++ app/routes/manager_routes.py
@@ -1,52 +1,53 @@
 from flask import Blueprint, jsonify, request
 from ..utils.route_to_apollo_utils import enrich_company, search_people
-from ..utils.manager_utils import strip_enrichment_json
+from ..utils.manager_utils import strip_enrichment_json, strip_enrichment_json_to_dict
 
 bp = Blueprint("manager", import_name="manager-routes", url_prefix="/handler")
 
 # need to break this up into a few utility functions -- too complicated at this point
 @bp.route("/hello", methods = ["POST"])
 def handler():
-
-    #we get the domains and the titles from the frontend
-    input_from_front_end = request.get_json()
-    domains = []
-    domains.extend((input_from_front_end["domainList"]).split(",")) # need to comma separate raw string
-    titles = input_from_front_end["jobTitlesList"].split(",") # need to comma separate raw string
-    hubspot_use_only_new_contacts = input_from_front_end["useOnlyNewContacts"]
-    max_dollars = input_from_front_end["maxDollars"]
+    processed_input = process_input_json(request.get_json())
+    domains = processed_input["domains"]
     
-    people_search_domains = '\n'.join(domains)
-    enriched_organizations = enrich_company(domains, "bulk")
-    enriched_organizations = strip_enrichment_json(enriched_organizations) #should work now
+    enriched_organizations = enrich_company(domains, "bulk") #dict
+    enriched_organizations = strip_enrichment_json(enriched_organizations) # returns list
+    enriched_org_lookup = strip_enrichment_json_to_dict(enriched_organizations) # returns domain-indexed dict of dicts
 
-    
-    people_search_dict = {
-        "q_organization_domains": people_search_domains,
-        "page": 1,
-        "per_page": 5,
-        "person_titles": titles,
-    }
 
-    people = search_people(people_search_dict) # we get the people from search
+    #people is a dict 
+    people = search_people(gen_people_search_dict(domains = domains, job_titles = processed_input[job_titles]))
 
-    enriched_org_lookup = {
-        org["domain"]: {"name": org["name"], "address": org["address"]} # getting stuck here
-        for org in enriched_organizations # iterate over list -> convert into a dict keyed by a domain
-    }
 
-    for orgs in enriched_organizations:
-        enriched_org_lookup["domain"] = {}
+    # note for 7/7 need to continue refactor -- this is a mess
 
 
     join = [] # empty list of enriched people
 
     for person in people:
         print(person)
-        if person["domain"] in enriched_org_lookup: # this will probably fail
+        if person["domain"] in enriched_org_lookup.keys(): 
             merge = {**person, **enriched_org_lookup[person["domain"]]}
             join.append(merge) #add the dict that merges person with org
 
     print(join)
 
     return people, 200
+
+
+def process_input_json(request: Response) -> dict[str]:
+    input_json = request.get_json()
+    domains = (input_json["domainList"]).split(",")
+    job_titles = (input_json["jobTitlesList"]).split(",")
+    hubspot_use_only_new_contacts = input_json["useOnlyNewContacts"]
+    max_dollars = input_from_front_end["maxDollars"]
+    return {"domains": domains, "job_titles": job_titles, "max_dollars": max_dollars, "hubspot_use_only_new_contacts": hubspot_use_only_new_contacts} 
+
+def gen_people_search_dict(domains: list[str], job_titles: list[str], page: int = 1, per_page: int = 10):
+    people_search_dict = {
+        "q_organization_domains" : '\n'.join(domains),
+        "page": page,
+        "per_page": per_page,
+        "person_titles": job_titles
+    }
+    return people_search_dict
--- app/utils/manager_utils.py
+++ app/utils/manager_utils.py
@@ -9,17 +9,22 @@ def construct_domain_enrichment_json(domains: list[str]) -> dict[str]:
     return return_dict
 
 # this can definitely be simpler!!!
-def strip_enrichment_json(pydict: dict[str], fields: list[str] = ["name", "street_address", "postal_code", "city", "state", "country",
-                                               "linkedin_uid", "linkedin_url", "id", "primary_domain"], organization_domain: bool = True) -> dict[str]:
-    pydict_org_list = pydict["organizations"]
-    return_dict = defaultdict(list)
-    for i in range(0, len(pydict_org_list)): # iterate over list of dicts
-        temp_dict_copy = defaultdict(list)
-        print(pydict_org_list[i])
-        for field in fields: #for each desired output
-            temp_dict_copy[field] = pydict_org_list[i][field]
-        if(organization_domain):
-            temp_dict_copy["domain"] = pydict_org_list[i]["primary_domain"]
-        return_dict[i] = temp_dict_copy
+def strip_enrichment_json(enriched_organizations: dict[str], fields: list[str] = ["name", "street_address", "postal_code", "city", "state", "country",
+                                               "linkedin_uid", "linkedin_url", "id", "primary_domain"], organization_domain: bool = True) -> list[str]:
+    '''pass in a dict with a nested list, return the list, albeit with fewer fields'''    
+    enriched_organizations = enriched_organizations["organizations"] #just get the nested list of dicts that's called "organizations"
+    return_list = []
+     for orgs in enriched_organizations:
+        temp_dict = defaultdict(str) # for overwriting orgs
+        for each_field in fields:
+            temp_dict[each_field] = orgs[each_field] # {each_field: orgs[each_field], ... }
+        return_list.append(temp_dict)
+    return return_list
+
+def strip_enrichment_json_to_dict(orgs_list: list[str]) -> dict[str]:
+    '''takes list of dicts and changes to a dict of dicts, where domain is the key'''
+    return_dict = defaultdict(dict)
+    for orgs in orgs_list:
+        domain = orgs.pop("domain")
+        return_dict[domain] = orgs
     return return_dict
-    
